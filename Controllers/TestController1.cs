c7eccc9326cb0b0eb0ea891f820c7b12815fbf32
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-17 14:07:32 +0530

    Trying to add three more features on the home page in the nav bar

---
+++ Controllers/TestController1.cs
@@ -0,0 +1,23 @@
+﻿using IhsanWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IhsanWeb.Controllers
+{
+    public class TestController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Test()
+        {
+            var testItems = new List<TestItems>()
+            {
+                new() {Id = 1, TestName = "Testing mic", TestStatus = "Completed" },
+                new() {Id = 2, TestName = "Testing camera", TestStatus = "Completed"},
+                new() {Id = 3, TestName = "Testing screen", TestStatus = "Not completed"}
+            };
+            return View(testItems);
+        }
+    }
+}
---
+++ Models/TestItems.cs
@@ -0,0 +1,9 @@
+﻿namespace IhsanWeb.Models
+{
+    public class TestItems
+    {
+        public int Id { get; set; }
+        public string TestName { get; set; } = string.Empty;
+        public string TestStatus { get; set; } = string.Empty;
+    }
+}

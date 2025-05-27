30c2b312457c3383261dfe4ebd2f58d5ee366dd3
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-27 15:40:55 +0100

    Working on the repository file

--- Repository/ProductRepository.cs
+++ Repository/ProductRepository.cs
@@ -21,6 +21,7 @@ namespace IhsanWeb.Repository
         }
 
         public void Update(UpdateProductViewModel viewModel)
+        {
             var product = _products.FirstOrDefault(x => x.Name == viewModel.Name);
             if (product is null)
             {

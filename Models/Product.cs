f93af8c4c6f7f266c63dfb03f35f8b5e2283339f
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-28 14:37:55 +0100

    Trying to rewrite the app

--- Models/Product.cs
+++ Models/Product.cs
@@ -4,7 +4,6 @@
     {
         public int ID {  get; set; }
         public string Name { get; set; } = default!;
-        public int Age { get; set; }
         public decimal Price {  get; set; }
         public string? Description { get; set; }
     }

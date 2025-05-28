38657791b3a3f19b6b7902dbb352be66faae7d9a
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2025-05-28 14:30:05 +0100

    Trying to rewrite the app

--- ViewModel/CreateProductViewModel.cs
+++ ViewModel/CreateProductViewModel.cs
@@ -6,8 +6,6 @@ namespace IhsanWeb.ViewModel
     {
         [Required (ErrorMessage = "Awfar relax o! At least add product name na.... ")]
         public string Name { get; set; } = string.Empty;
-        [Required(ErrorMessage = "Awfar relax o! Write product name joor")]
-        public string Name { get; set; } = string.Empty;
         [StringLength(200)]
         public string Description { get; set; } = string.Empty;
         [Required(ErrorMessage = "Boss even if na free you want sell am, add 0 to the price. No dey act like say... make I no talk sha.")]
